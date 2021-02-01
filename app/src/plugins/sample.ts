397dd83cb5d5f40f70ef812ca3053e536814afa0
y-u-y-a <yuya.ymd00@gmail.com>
2021-02-01 14:35:15 +0900

    sampleファイル削除

--- app/src/plugins/sample.ts
+++
@@ -1,9 +0,0 @@
-// import axios from 'axios';
-
-// export const axiosAuth = axios.create({
-//   baseURL: 'https://identitytoolkit.googleapis.com/v1'
-// });
-
-// export const axiosToken = axios.create({
-//   baseURL: 'https://securetoken.googleapis.com/v1'
-// });
